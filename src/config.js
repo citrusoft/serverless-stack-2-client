be3237eb69f70b259c1f6d80df254e4f8b2d2753
Thomas Hunt <thunt@citrusoft.com>
2019-07-26 09:42:49 -0400

    master: updating dev with new Stack details.

--- src/config.js
+++ src/config.js
@@ -2,32 +2,32 @@ const dev = {
   STRIPE_KEY: "pk_test_1BsF5o5Jifk1CaJJMmpKBwNi",
   s3: {
     REGION: "us-east-1",
-    BUCKET: "notes-app-2-api-dev-attachmentsbucket-unfh6ryhf98n"
+    BUCKET: "notes-app-2-api-dev-attachmentsbucket-1btda9d6h8mfd"
   },
   apiGateway: {
     REGION: "us-east-1",
-    URL: "https://smgiw2aqg1.execute-api.us-east-1.amazonaws.com/dev"
+    URL: "https://ewgyx18qk1.execute-api.us-east-1.amazonaws.com/dev"
   },
   cognito: {
     REGION: "us-east-1",
-    USER_POOL_ID: "us-east-1_CsDoGUX2C",
-    APP_CLIENT_ID: "13s0uq2miosplm5k6f5qohsfo8",
-    IDENTITY_POOL_ID: "us-east-1:4c7640bd-afff-4820-a34e-dc0880915ec3"
+    USER_POOL_ID: "us-east-1_FKhbB3IjK",
+    APP_CLIENT_ID: "782i6e3vkfsk4fhn25piin2kt",
+    IDENTITY_POOL_ID: "us-east-1:9c228e25-580d-4c66-ada3-4f80cd7303ed"
   }
 };
 
 const prod = {
   STRIPE_KEY: "pk_test_1BsF5o5Jifk1CaJJMmpKBwNi",
   s3: {
-    REGION: "us-east-1",
+    REGION: "us-east-2",
     BUCKET: "notes-app-2-api-prod-attachmentsbucket-8xcbrmnbychh"
   },
   apiGateway: {
-    REGION: "us-east-1",
+    REGION: "us-east-2",
     URL: "https://eq93zwm3kd.execute-api.us-east-1.amazonaws.com/prod"
   },
   cognito: {
-    REGION: "us-east-1",
+    REGION: "us-east-2",
     USER_POOL_ID: "us-east-1_DkuiIdfwW",
     APP_CLIENT_ID: "34kkpqv9htg0013i8gl934leo0",
     IDENTITY_POOL_ID: "us-east-1:0314f685-0c4b-4119-b0c0-b783542a0da8"
